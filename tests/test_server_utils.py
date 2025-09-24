a6a7f5c23f48fb52ede6692ddfbb8450892ff69f
wowjinxy <wowjinxy@aol.com>
2025-09-24 00:42:18 -0500

    Expand Discord management tool coverage

---
+++ tests/test_server_utils.py
@@ -0,0 +1,80 @@
+import pytest
+
+from discord_mcp.server import (
+    DiscordToolError,
+    _parse_colour,
+    _parse_optional_bool,
+    _parse_permissions,
+)
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        (True, True),
+        (False, False),
+        ("true", True),
+        ("NO", False),
+        (1, True),
+        (0, False),
+        (None, None),
+    ],
+)
+def test_parse_optional_bool_valid(value, expected):
+    assert _parse_optional_bool(value, "test") == expected
+
+
+@pytest.mark.parametrize("value", ["maybe", 2, object()])
+def test_parse_optional_bool_invalid(value):
+    with pytest.raises(DiscordToolError):
+        _parse_optional_bool(value, "test")
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ("#FF0000", 0xFF0000),
+        ("0x00ff00", 0x00FF00),
+        ("default", 0x000000),
+        (16711935, 0xFF00FF),
+    ],
+)
+def test_parse_colour_valid(value, expected):
+    colour = _parse_colour(value, name="color")
+    assert colour is not None
+    assert colour.value == expected
+
+
+def test_parse_colour_named():
+    colour = _parse_colour("blurple", name="color")
+    assert colour is not None
+    # discord.py's blurple constant is 0x5865F2
+    assert colour.value == 0x5865F2
+
+
+@pytest.mark.parametrize("value", ["not-a-colour", -1])
+def test_parse_colour_invalid(value):
+    with pytest.raises(DiscordToolError):
+        _parse_colour(value, name="color")
+
+
+def test_parse_permissions_from_list():
+    perms = _parse_permissions(["manage_roles", "kick_members"], None)
+    assert perms is not None
+    assert perms.manage_roles and perms.kick_members
+    assert not perms.administrator
+
+
+def test_parse_permissions_from_value():
+    perms = _parse_permissions(None, "1049600")
+    assert perms is not None
+    assert isinstance(perms.value, int)
+
+
+def test_parse_permissions_unknown_name():
+    with pytest.raises(DiscordToolError):
+        _parse_permissions(["not_a_permission"], None)
+
+
+def test_parse_permissions_none():
+    assert _parse_permissions(None, None) is None
